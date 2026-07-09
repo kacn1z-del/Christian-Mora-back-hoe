defa0ce5d43b8184c342dff9e3bf99645ffe5b55
kacn1z-del <kacn1z@gmail.com>
2026-07-08 22:29:08 -0600

    Crear coverage.js

---
+++ src/data/coverage.js
@@ -0,0 +1,11 @@
+/* TODO: ajustar esta lista a las zonas reales donde trabaja Christian Mora Maquinaria Pesada. */
+export const COBERTURA = [
+  'San José',
+  'Alajuela',
+  'Cartago',
+  'Heredia',
+  'Acosta',
+  'Desamparados',
+  'Aserrí',
+  'Puriscal',
+]
